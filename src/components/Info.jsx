52dee885fb70ce02e0f5ec920a5c0d21d5809974
greenspantom <video_reportage@proton.me>
2025-08-21 16:04:07 +0200

    Update Info.jsx

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,8 +1,8 @@
 export default function Info() {
   return (
-    <section id="STORY-STORY-STORY" className="py-16">
+    <section id="Info" className="py-16">
       <div className="max-w-4xl mx-auto px-4 text-center">
-        <h2 className="text-2xl md:text-3xl font-bold mb-4">Info</h2>
+        <h2 className="text-2xl md:text-3xl font-bold mb-4">STORY-STORY-STORY</h2>
         <p className="text-gray-300">
           Als er iets is wat ik door al die jaren heb geleerd is dat alles draait rond een goed verhaal. En ik hou ervan om dat verhaal in beeld te brengen. Dus ben je nog op zoek naar een verhalenverteller, regisseur of cameraman/monteur om je verhaal vorm te geven , dan help ik maar al te graag.  Dit kan als een one-man-band , maar ook als deel van een groter geheel.  Televisie, online, campagnes, corporate of iets heel persoonlijks, het kan allemaal. Want verhalen zijn niet alleen overal maar bovenal onmisbaar in onze samenleving.
         </p>
